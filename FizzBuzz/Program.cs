f6e30cc7ae3a104070eed02c29f7ecec64215c88
Ethan Martin <Ethan.Martin@softwire.com>
2019-09-03 15:43:10 +0100

    Fizz Buzz Part 2

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -12,22 +12,48 @@ namespace FizzBuzz
         {
             for (int number = 1; number <= 100; number++)
             {
-                string result = "";
-                if (IsDivisibleBy(number, 3))
-                {
-                    result += "Fizz";
-                }
-                if (IsDivisibleBy(number, 5))
-                {
-                    result += "Bang";
-                }
-                if (result == "")
-                {
-                    result = number.ToString();
-                }
+                string result = BuildString(number);
                 Console.WriteLine(result);
             }
-            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+
+        static string BuildString(int number)
+        {
+            List<string> codes = new List<string>();
+            if (IsDivisibleBy(number, 3))
+            {
+                codes.Add("Fizz");
+            }
+            if (IsDivisibleBy(number, 13))
+            {
+                // Goes immediately in front of any B phrase 
+                codes.Add("Fezz");
+            }
+            if (IsDivisibleBy(number, 5))
+            {
+                codes.Add("Buzz");
+            }
+            if (IsDivisibleBy(number, 7))
+            {
+                codes.Add("Bang");
+            }
+            if (IsDivisibleBy(number, 11))
+            {
+                return "Bong";
+            }
+            if (IsDivisibleBy(number, 17))
+            {
+                codes.Reverse();
+            }
+
+            string result = String.Join("", codes);
+
+            if (result == "")
+            {
+                result = number.ToString();
+            }
+            return result;
         }
 
         static bool IsDivisibleBy(int divisend, int divisor)
