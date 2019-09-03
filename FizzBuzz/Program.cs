63631f6954c62d30e3852b84aa57c9bd72070762
Ethan Martin <Ethan.Martin@softwire.com>
2019-09-03 15:08:13 +0100

    Fizz Bang Part1

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,29 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            for (int number = 1; number <= 100; number++)
+            {
+                string result = "";
+                if (IsDivisibleBy(number, 3))
+                {
+                    result += "Fizz";
+                }
+                if (IsDivisibleBy(number, 5))
+                {
+                    result += "Bang";
+                }
+                if (result == "")
+                {
+                    result = number.ToString();
+                }
+                Console.WriteLine(result);
+            }
+            Console.WriteLine("end");
+        }
+
+        static bool IsDivisibleBy(int divisend, int divisor)
+        {
+            return ((divisend % divisor) == 0);
         }
     }
 }
